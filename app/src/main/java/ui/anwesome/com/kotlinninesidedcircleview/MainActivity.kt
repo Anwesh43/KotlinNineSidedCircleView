4cfa628250b756f0f72ccffa76defbb1f6cf6d3d
Anwesh43 <anweshthecool0@gmail.com>
2018-01-01 03:11:44 +0530

    using view in activity

--- app/src/main/java/ui/anwesome/com/kotlinninesidedcircleview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinninesidedcircleview/MainActivity.kt
@@ -2,11 +2,13 @@ package ui.anwesome.com.kotlinninesidedcircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.ninesidedballview.NineSidedBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        NineSidedBallView.create(this)
     }
 }
