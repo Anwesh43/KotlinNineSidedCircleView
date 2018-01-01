a3fe5f80c8e617fcd731d3d81993bc7a65babdd3
Anwesh43 <anweshthecool0@gmail.com>
2018-01-01 10:45:10 +0530

    using on expandlistener in activity

--- app/src/main/java/ui/anwesome/com/kotlinninesidedcircleview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinninesidedcircleview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlinninesidedcircleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.ninesidedballview.NineSidedBallView
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        NineSidedBallView.create(this)
+        val view =  NineSidedBallView.create(this)
+        view.setOnExpandCollapseListener({
+            Toast.makeText(this,"expanded",Toast.LENGTH_SHORT).show()
+        },{
+            Toast.makeText(this,"collapsed",Toast.LENGTH_LONG).show()
+        })
     }
 }
